d323b2d185c9193f248db31b7d05ce9850e65fe3
Antonela Borgogno <antonelaborgogno@gmail.com>
2025-12-05 23:07:43 -0300

    quitar validateMiddleware de update

--- src/routes/quotes-router.ts
+++ src/routes/quotes-router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllQuotes, getQuoteById, creareQuote, updateQuote, deleteQuote } from "../controllers/quotes-controller";
+import { getAllQuotes, getQuoteById, createQuote, updateQuote, deleteQuote } from "../controllers/quotes-controller";
 import { validateQuote } from "../middlewares/validate-middleware"; // importacion de middleware que controla el contenido de las frases
 
 // creamos la instancia de Router
@@ -7,8 +7,8 @@ const router: Router = Router();
 
 router.get('/', getAllQuotes);
 router.get('/:id', getQuoteById);
-router.post('/', validateQuote, creareQuote);
-router.patch('/:id', validateQuote, updateQuote);
+router.post('/', validateQuote, createQuote);
+router.patch('/:id', updateQuote);
 router.delete('/:id', deleteQuote);
 
 export default router;
